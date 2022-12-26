5cb2168ca91edbd5110fd8b7eb64b9bf483cdf1d
egoekalp <erditaner.goekalp@eqs.com>
2022-12-26 18:38:08 +0100

    chore(angular-v12): app
    add some imports from rxjs for bundle size testing

--- angular-v12/src/app/app.component.ts
+++ angular-v12/src/app/app.component.ts
@@ -1,7 +1,9 @@
-import { Component, Inject, OnInit } from '@angular/core';
+import { Component, Inject, OnDestroy, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { sortBy } from 'lodash';
 // eslint-disable-next-line import/no-namespace
 import * as moment from 'moment';
+import { Subject, takeUntil } from 'rxjs';
 
 import { Environment } from './app.model';
 
@@ -10,15 +12,24 @@ import { Environment } from './app.model';
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
+    readonly destroyed$ = new Subject<void>();
+
     readonly title = 'App component';
 
-    constructor(@Inject('env') readonly environment: Environment) {}
+    constructor(private readonly router: Router, @Inject('env') readonly environment: Environment) {}
 
     ngOnInit(): void {
         // eslint-disable-next-line no-console
         console.log('moment', moment);
         // eslint-disable-next-line no-console
         console.log('sortBy', sortBy);
+        // eslint-disable-next-line no-console
+        this.router.events.pipe(takeUntil(this.destroyed$)).subscribe(console.log);
+    }
+
+    ngOnDestroy(): void {
+        this.destroyed$.next();
+        this.destroyed$.complete();
     }
 }
