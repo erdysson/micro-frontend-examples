d498f49ef5f194a98e69035c9622d6379c00378f
egoekalp <erditaner.goekalp@eqs.com>
2022-12-30 17:45:19 +0100

    fix(micro-frontend-examples): v15 remote
    fix routing issues

--- angular-versions-mix/angular-v15/src/app/app.component.ts
+++ angular-versions-mix/angular-v15/src/app/app.component.ts
@@ -1,7 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'app-root',
     template: '<router-outlet></router-outlet>',
 })
-export class AppComponent {}
+export class AppComponent implements OnInit, OnDestroy {
+    constructor(private readonly router: Router) {
+        this.onPopState = this.onPopState.bind(this);
+    }
+
+    ngOnInit(): void {
+        this.onPopState();
+        window.addEventListener('popstate', this.onPopState);
+    }
+
+    ngOnDestroy(): void {
+        window.removeEventListener('popstate', this.onPopState);
+    }
+
+    private onPopState(): void {
+        void this.router.navigateByUrl(location.pathname.slice(1));
+    }
+}
